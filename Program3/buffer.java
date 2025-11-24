2b4cb7b88d13d7a9e00b8d7e88c434390396eaa1
ankith5980 <ankithpratheesh147@gmail.com>
2025-11-24 23:40:42 +0530

    Program 3

--- Program3/buffer.java
+++ Program3/buffer.java
@@ -1,7 +1,20 @@
-// Implement String and StringBuffer classes. 
+// Implement String and StringBuffer classes.
 
 package Program3;
 
-public class buffer {
-    
+public class Buffer {
+    public static void main(String[] args) {
+        // String (immutable)
+        String s = "Hello";
+        System.out.println("Original String: " + s);
+        String s2 = s.concat(" World"); // returns a new String
+        System.out.println("After concat -> original: " + s);
+        System.out.println("After concat -> new: " + s2);
+
+        // StringBuffer (mutable)
+        StringBuffer sb = new StringBuffer("Hello");
+        System.out.println("Original StringBuffer: " + sb);
+        sb.append(" World"); // mutates same object
+        System.out.println("After append -> same object: " + sb);
+    }
 }
