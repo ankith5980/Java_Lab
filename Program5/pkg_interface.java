252efbae24c6b10b35d90534e98c671bb71db7fa
ankith5980 <ankithpratheesh147@gmail.com>
2025-11-27 11:29:38 +0530

    Program 5

--- Program5/pkg_interface.java
+++ Program5/pkg_interface.java
@@ -3,5 +3,31 @@
 package Program5;
 
 public class pkg_interface {
-    
+    public static void main(String[] args) {
+        System.out.println("Demonstrating Package and Interface");
+        System.out.println("Current Package: Program5");
+        
+        Car myCar = new Car();
+        myCar.start();
+        myCar.stop();
+    }
+}
+
+// Interface declaration
+interface Vehicle {
+    void start();
+    void stop();
+}
+
+// Class implementing the interface
+class Car implements Vehicle {
+    @Override
+    public void start() {
+        System.out.println("Car started.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Car stopped.");
+    }
 }
